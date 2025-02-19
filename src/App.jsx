cb1431cde087f0c127d12b08a07d613df2456e08
TanDuy274 <phanduy27042004@gmail.com>
2025-02-19 19:03:05 +0700

    them minh chung

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import "./App.css";
-import Button from "./components/Button";
-// import Calculator from "./components/Calculator/Calculator";
-// import HelloUser from "./components/HelloUser";
+import Calculator from "./components/Calculator/Calculator";
 
 function App() {
-  return <Button color="primary" />;
+  return <Calculator />;
 }
 
 export default App;
