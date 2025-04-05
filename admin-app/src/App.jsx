8b2d461f27b31979dba8b924b7e38f3382c274ea
TanDuy274 <phanduy27042004@gmail.com>
2025-04-05 23:22:12 +0700

    api overview

--- admin-app/src/App.jsx
+++ admin-app/src/App.jsx
@@ -1,8 +1,22 @@
 import { Navigate, Route, Routes } from "react-router";
 import "./App.css";
-import Layout from "./layout/Layout";
+import OverviewItem from "./components/OverviewItem/OverviewItem";
 
 function App() {
+  const OVERVIEW_URL = "http://localhost:3000/overview";
+  const [overviewData, setOverviewData] = useState();
+
+  useEffect(() => {
+    fetch(OVERVIEW_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        setOverviewData(data), console.log(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
   return (
     <>
       <Routes>
