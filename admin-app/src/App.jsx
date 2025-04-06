aa02cff703f529bc8ea5cea9747259ded4e39700
TanDuy274 <phanduy27042004@gmail.com>
2025-04-06 21:21:58 +0700

    datatable react js

--- admin-app/src/App.jsx
+++ admin-app/src/App.jsx
@@ -1,21 +1,67 @@
 import { Navigate, Route, Routes } from "react-router";
 import "./App.css";
 import OverviewItem from "./components/OverviewItem/OverviewItem";
+import $ from "jquery";
+import "datatables.net-dt";
+import "datatables.net-dt/css/dataTables.dataTables.min.css";
 
 function App() {
   const OVERVIEW_URL = "http://localhost:3000/overview";
   const [overviewData, setOverviewData] = useState();
+  const tableRef = useRef();
 
   useEffect(() => {
     fetch(OVERVIEW_URL)
       .then((response) => response.json())
       .then((data) => {
-        setOverviewData(data), console.log(data);
+        setOverviewData(data);
+        console.log(data);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
   }, []);
+
+  useEffect(() => {
+    if (tableRef.current) {
+      $(tableRef.current).DataTable(); // Khởi tạo DataTable
+    }
+  }, []);
+
+  const tableData = [
+    [
+      "Tiger Nixon",
+      "System Architect",
+      "Edinburgh",
+      "5421",
+      "2011-04-25",
+      "$320,800",
+    ],
+    [
+      "Garrett Winters",
+      "Accountant",
+      "Tokyo",
+      "8422",
+      "2011-07-25",
+      "$170,750",
+    ],
+    [
+      "Ashton Cox",
+      "Junior Technical Author",
+      "San Francisco",
+      "1562",
+      "2009-01-12",
+      "$86,000",
+    ],
+    [
+      "Cedric Kelly",
+      "Senior Javascript Developer",
+      "Edinburgh",
+      "6224",
+      "2012-03-29",
+      "$433,060",
+    ],
+  ];
 
   return (
     <>
